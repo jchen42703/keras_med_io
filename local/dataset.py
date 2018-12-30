7a33c1adf80d9b629ef3c8996be7b3ddfb2953dd
Joseph <jchen42703@gmail.com>
2018-12-30 06:43:15 -0500

    Rewrote it to load .npy files easily and serve with data aug easily

--- local/dataset.py
+++ local/dataset.py
@@ -1,29 +1,42 @@
 import tensorflow as tf
-import SimpleITK as sitk
 import numpy as np
 import os
-import json
 import argparse
 import logging
-from helperfunc import *
-# from utils.sitk_utils import resample_to_spacing
-from utils.utils import start_logger
-from keras_med_io.utils.io_func import get_multi_class_labels, resample_img
+from keras_med_io.local.helperfunc import *
 from glob import glob
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 class TFRdataset(object):
     '''
     Goal: To read all the preprocessed .npy patches and make a tfrecords dataset
+     -> Assumes file structure:
+        * in_dir
+            -> saves tfrecord here
+            * imagesTr
+            * labelsTr
+     -> Assumes that files/labels have same corresponding names
     '''
-    def __init__(self, in_dirs):
+    def __init__(self, in_dir, n_dim, shape):
         self.in_dir = in_dir
-        self.dset_path = os.path.join(self.fpath, 'dataset.tfrecord')
-        dformat = '*.npy'
-        self.inputs_paths = glob(os.path.join(self.in_dir, 'imageTr'), recursive = True)
-        self.labels_paths = glob(os.path.join(self.in_dir, 'labelsTr'), recursive = True)
+        self.n_dim = n_dim
 
-    def make_train_dataset(self, save_interpolation=None, compression=None,
-                           output_path=None):
+        self.dset_path = os.path.join(self.in_dir, 'dataset.tfrecord')
+        dformat = '*.npy'
+        self.inputs_path = os.path.join(self.in_dir, 'imagesTr')
+        self.labels_path = os.path.join(self.in_dir, 'labelsTr')
+        self.ids = os.listdir(self.labels_path) #assumes that inputs and labels have same names
+        np.random.shuffle(self.ids)
+        # should include both channels (...., n_channels)
+        self.shape = shape
+        # assumes all .npy arrays have the same shape
+
+    def make_train_dataset(self, compression=None, output_path=None):
         # initialising the tfrecord writer
         if compression:
             options = tf.python_io.TFRecordOptions(
@@ -37,10 +50,10 @@ class TFRdataset(object):
         holder = {}
         logging.info('Constructing training dataset')
         # loading and writing
-        for img_pair in zip(self.inputs_paths, self.labels_paths):
-            holder['idx'] = img_pair[0].split('./')[-1]
-            holder['image'] = np.load(img_pair[0])
-            holder['label'] = np.load(img_pair[1])
+        for id in self.ids:
+            holder['idx'] = id
+            holder['image'] = np.expand_dims(np.load(os.path.join(self.inputs_path, id)), 0) # adding batch_size dim
+            holder['label'] = np.expand_dims(np.load(os.path.join(self.labels_path, id)), 0)
             self.write_to_tfrecord(holder)
             logging.info(holder['idx'] + ' processed')
         self.writer.close()
@@ -48,7 +61,7 @@ class TFRdataset(object):
     def write_to_tfrecord(self, holder):
         # type checking
         try:
-            assert(holder[0].dtype == np.float32)
+            assert(holder['image'].dtype == np.float32)
         except AssertionError:
             logging.info(holder['idx'] + ': np.float32 expected, got {}, \
             converting to np.float32'.format(holder['image'].dtype))
@@ -62,29 +75,19 @@ class TFRdataset(object):
             holder['label'] = holder['label'].astype(np.uint8)
 
         # shape checking
-        if len(holder['image'].shape) == 4:
-            try:
-                assert(holder['image'].shape[:-1]
-                       == holder['label'].shape[:-1])
-            except AssertionError:
-                logging.warning(holder['idx'] + ':expected shapes to be \
-                                equal, but image was {} and label was {}'.format(
-                                    holder['image'].shape[:-1],
-                                    holder['label'].shape[:-1]))
-        else:
-            try:
-                assert(holder['image'].shape == holder['label'].shape)
-            except AssertionError:
-                logging.warning(holder['idx'] + ':expected shapes to be \
-                                equal, but image was {} and label was {}'.format(
-                                    holder['image'].shape,
-                                    holder['label'].shape))
+        try:
+            assert(holder['image'].shape[:-1]
+                   == holder['label'].shape[:-1])
+        except AssertionError:
+            logging.warning(holder['idx'] + ':expected shapes to be \
+                            equal, but image was {} and label was {}'.format(
+                                holder['image'].shape[:-1],
+                                holder['label'].shape[:-1]))
 
         raw_image = holder['image'].tobytes()
         raw_label = holder['label'].tobytes()
         shape_i = np.array(holder['image'].shape, np.int32).tobytes()
         shape_l = np.array(holder['label'].shape, np.int32).tobytes()
-
         # 32bit constrained from google protobof
         Kint32Max = 2147483647.0
 
@@ -109,13 +112,12 @@ class TFRdataset(object):
                     }))
         self.writer.write(entry.SerializeToString())
 
-    def provide_tf_dataset(self, number_epochs, shape=None, batch_size=1,
+    def provide_tf_dataset(self, number_epochs, batch_size=1,
                            num_parallel_calls=8, mode='train', fraction=0.0,
                            shuffle_size=100, variance=0.1, compression=None,
                            multi_GPU=False):
 
         self.mode = mode
-        self.shape = shape
         self.batch_size = batch_size
         self.fraction = fraction
         self.variance = variance
@@ -124,6 +126,7 @@ class TFRdataset(object):
             compression_type_ = 'GZIP'
         else:
             compression_type_ = ''
+
         dataset = tf.data.TFRecordDataset(
                     self.dset_path, compression_type=compression_type_)
         dataset = dataset.apply(
@@ -139,13 +142,15 @@ class TFRdataset(object):
 
         if multi_GPU:
             return dataset
-
         else:
             iterator = dataset.make_one_shot_iterator()
             inputs, outputs = iterator.get_next()
             return inputs, outputs
 
     def parse_image(self, entry):
+        '''
+        Decodes the TFRecordDataset and does data aug
+        '''
         parsed = tf.parse_single_example(entry, features={
                     'image': tf.FixedLenFeature([], tf.string),
                     'shape_i': tf.FixedLenFeature([], tf.string),
@@ -170,56 +175,58 @@ class TFRdataset(object):
         mask = tf.decode_raw(parsed['label'], tf.uint8)
         mask = tf.reshape(mask, shape_l)
 
-        image, mask, rval = tf.py_func(
-                                        transforms,
-                                        [image, mask, self.shape, self.fraction, self.spacing, self.variance],
-                                        Tout=[tf.float32, tf.float32, tf.float64])
-        cropped_s = tf.cast(cropped_s, tf.uint8)
-
+        # data aug
+        image, mask = tf.py_func(
+                                 transforms,
+                                 [image, mask, self.n_dim , self.fraction, self.variance],
+                                 Tout=[tf.float32, tf.float32])
+        mask = tf.cast(mask, tf.uint8)
         # static output shape required by tf.dataset.batch() method!
-        cropped_i.set_shape([self.num_modalities] + self.shape)
-        cropped_s.set_shape([self.num_classes] + self.shape)
-        print("input shape" + str(cropped_i.shape))
-        print("segmentation shape" + str(cropped_s.shape))
+        # assumes shape (..., n_channels)
+        image.set_shape(self.shape)
+        mask.set_shape(self.shape)
 
+        print("input shape" + str(image.shape))
+        print("segmentation shape" + str(image.shape))
+
+        # what's the difference??
         if self.mode == 'train':
-            return({'img': cropped_i, 'object_in_crop': objects_in_crop},
-                   cropped_s)
+            return(image, mask)
         elif self.mode == 'inference':
-            tf.Print(['img shape'], [tf.shape(stacked[:self.num_modalities])])
-            return({'img': cropped_i, 'rval': rval},
-                   cropped_s)
+            # tf.Print(['img shape'], [tf.shape(stacked[:self.num_modalities])])
+            return(image, mask)
 
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser(description='generate dataset for'
-                                     'MSD_challenge')
-    parser.add_argument('-i', '--input', help='input (root) path',
-                        required=True)
-    parser.add_argument('-o', '--output',
-                        help='path to store generated TFRecord and json',
-                        required=True)
-    parser.add_argument('-s', '--save_interpolation',
-                        help='save interpolated images',
-                        required=False,
-                        default=None)
-    parser.add_argument('-c', '--compression',
-                        help='compress generated TFRecord',
-                        required=False,
-                        default=None)
-
-    args = vars(parser.parse_args())
-
-    fpath = args['input']
-    output_path = args['output']
-    compression = args['compression']
-    save_images = args['save_interpolation']
-
-    start_logger(os.path.join(fpath + 'dataset_generation'))
-    logger = logging.getLogger(__name__)
-    dset = TFRdataset(fpath)
-    if dset.spacing is None or dset.shapes is None:
-        dset.scan_dataset_spacing_shapes()
-    dset.make_train_dataset(output_path=output_path,
-                            compression=compression,
-                            save_interpolation=save_images)
+# if __name__ == '__main__':
+#
+#     parser = argparse.ArgumentParser(description='generate dataset for'
+#                                      'MSD_challenge')
+#     parser.add_argument('-i', '--input', help='input (root) path',
+#                         required=True)
+#     parser.add_argument('-o', '--output',
+#                         help='path to store generated TFRecord and json',
+#                         required=True)
+#     parser.add_argument('-s', '--save_interpolation',
+#                         help='save interpolated images',
+#                         required=False,
+#                         default=None)
+#     parser.add_argument('-c', '--compression',
+#                         help='compress generated TFRecord',
+#                         required=False,
+#                         default=None)
+#
+#     args = vars(parser.parse_args())
+#
+#     fpath = args['input']
+#     output_path = args['output']
+#     compression = args['compression']
+#     # save_images = args['save_interpolation']
+#
+#     start_logger(os.path.join(fpath + 'dataset_generation'))
+#     logger = logging.getLogger(__name__)
+#     dset = TFRdataset(fpath)
+#     if dset.spacing is None or dset.shapes is None:
+#         dset.scan_dataset_spacing_shapes()
+#     dset.make_train_dataset(output_path=output_path,
+#                             compression=compression,
+#                             )
+#                             # save_interpolation=save_images)
