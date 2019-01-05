8d34352fbd2b104fef3426613df85a1db654987f
Joseph <jchen42703@gmail.com>
2019-01-04 21:53:30 -0500

    Updating the documentation and fixing whitening

--- utils/io_func.py
+++ utils/io_func.py
@@ -3,58 +3,90 @@
 # funcions for quick testing
 import os
 from glob import glob
-
 import SimpleITK as sitk
 import numpy as np
 
 def to_do():
-    '''
+    """
     * histogram_normalization
     * resample for multiple channels with nibabel
-    '''
+    """
     return ()
+
 # helper functions
-def whitening(arr):
-    '''
-    Mean-Var Normalization
-    * mean of 0 and standard deviation of 1
-
-    param arr: numpy array to whiten
-    '''
-    return arr-np.mean(arr) / (np.std(arr))
-
-def normalize(arr, range = [0,1]):
-    '''
+def normalization(arr, normalize_mode, norm_range = [0,1]):
+    """
+    Helper function: Normalizes the image based on the specified mode and range
     Args:
         arr: numpy array
-        range: list of 2 integers specifying normalizing range
-            based on https://stats.stackexchange.com/questions/178626/how-to-normalize-data-between-1-and-1
+        normalize_mode: either "whiten", "normalize_clip", or "normalize" representing the type of normalization to use
+        norm_range: (Optional) Specifies the range for the numpy array values
     Returns:
-        Normalized array with outliers clipped in the specified range
-    '''
-    norm_img = (range[1]-range[0]) * (arr - np.amin(arr)) / (np.amax(arr) - np.amin(arr)) + range[0]
-    return norm_img
+        A normalized array based on the specifications
+    """
+    # reiniating the batch_size dimension
+    if normalize_mode == "whiten":
+        return whiten(arr)
+    elif normalize_mode == "normalize_clip":
+        return normalize_clip(arr,  norm_range = norm_range)
+    elif normalize_mode == "normalize":
+        return minmax_normalize(arr, norm_range = norm_range)
+    else:
+        return NotImplementedError("Please use the supported modes.")
 
-def normalize_clip(arr, range = [0,1]):
-    '''
+def normalize_clip(arr, norm_range = [0,1]):
+    """
     Args:
         arr: numpy array
-        range: list of 2 integers specifying normalizing range
+        norm_range: list of 2 integers specifying normalizing range
             based on https://stats.stackexchange.com/questions/178626/how-to-normalize-data-between-1-and-1
     Returns:
         Whitened and normalized array with outliers clipped in the specified range
-    '''
+    """
     # whitens -> clips -> scales to [0,1]
     if isinstance(arr,sitk.Image):
         arr = sitk.GetArrayFromImage(arr)
     # whiten
-    norm_img = np.clip((arr-np.mean(arr)) / (np.std(arr)),
-                        -5, 5)
-    norm_img = (range[1]-range[0]) * (norm_img - np.amin(norm_img)) / (np.amax(norm_img) - np.amin(norm_img)) + range[0]
+    norm_img = np.clip(whiten(arr), -5, 5)
+    norm_img = minmax_normalize(arr, norm_range)
+    return norm_img
 
+def whiten(arr):
+    """
+    Mean-Var Normalization
+    * mean of 0 and standard deviation of 1
+    Args:
+        arr: numpy array
+    Returns:
+        A numpy array with a mean of 0 and a standard deviation of 1
+    """
+    shape = arr.shape
+    arr = arr.flatten()
+    norm_img = (arr-np.mean(arr)) / np.std(arr)
+    return norm_img.reshape(shape)
+
+def minmax_normalize(arr, norm_range = [0,1]):
+    """
+    Args:
+        arr: numpy array
+        norm_range: list of 2 integers specifying normalizing range
+            based on https://stats.stackexchange.com/questions/178626/how-to-normalize-data-between-1-and-1
+    Returns:
+        Normalized array with outliers clipped in the specified range
+    """
+    norm_img = ((norm_range[1]-norm_range[0]) * (arr - np.amin(arr)) / (np.amax(arr) - np.amin(arr))) + norm_range[0]
     return norm_img
 
 def resample_img(itk_image, out_spacing=[1.0, 1.0, 1.0], is_label=False):
+    """
+    Resamples a SimpleITK image to the desired voxel spacing.
+    Args:
+        itk_image: a 2D/3D SimpleITK image
+        out_spacing: a list of the spacing
+        is_label: boolean on whether the itk_image is a label
+    Returns:
+        a resample SimpleITK image
+    """
     if isinstance(itk_image, np.ndarray):
         itk_image = sitk.GetImageFromArray(itk_image)
     # Resample images to 2mm spacing with SimpleITK
@@ -81,10 +113,16 @@ def resample_img(itk_image, out_spacing=[1.0, 1.0, 1.0], is_label=False):
 
     return resample.Execute(itk_image)
 
-def histogram_normalization():
-    return Exception("Not Implemented")
+# def histogram_normalization():
+#     return Exception("Not Implemented")
 
 def N4_bias_correction(img):
+    """
+    Args:
+        img: A SimpleITK image
+    Returns:
+        A bias corrected SimpleITK image
+    """
     # https://github.com/poornasandur/BRATS_N4Bias/blob/master/test3.py
     img=sitk.Cast(img,sitk.sitkFloat32)
     img_mask=sitk.Cast(sitk.BinaryNot(sitk.BinaryThreshold(img, 0, 0)), sitk.sitkUInt8)
@@ -92,99 +130,69 @@ def N4_bias_correction(img):
 
 def get_multi_class_labels(data, n_labels, labels=None, remove_background = False):
     """
-    Channels_last for multiclass.
-    **** Make sure to remove the background class dim (first dim of the output label)
-
-    Translates a label map into a set of binary labels.
-    :param data: numpy array containing the label map with shape: (n_samples, 1, ...).
-        ** if regular images are 4D, the labels should be expanded to 4D as well
-    :param n_labels: number of labels.
-    :param labels: integer values of the labels.
-    :param remove_background: option to drop the background mask (first label 0)
-    :return: binary numpy array of shape: (n_samples, n_labels, ...)
+    One-hot encodes a segmentation label.
+    Args:
+        data: numpy array containing the label map with shape: (n_samples,..., 1).
+        n_labels: number of labels
+        labels: list of the integer/float values of the labels
+        remove_background: option to drop the background mask (first label 0)
+    Returns:
+        binary numpy array of shape (n_samples,..., n_labels) or (n_samples,..., n_labels-1)
     """
 
     new_shape = data.shape + (n_labels,)
     y = np.zeros(new_shape, np.int8)
-    print(y.shape)
     for label_index in range(n_labels):
         if labels is not None:
+            # with the labels specified
             y[:,:,:, label_index][data == labels[label_index]] = 1
         else:
+            # automated
             y[:, :, :, label_index][data == (label_index + 1)] = 1
     if remove_background:
         without_background = n_labels - 1
-        y = y[-without_background:]
+        y = y[-without_background:] # removing the background
     return y
 
-############### utility
-def get_file_lists(data_dir):
-
-    # setting directories
-    dataset_train= 'imagesTr'
-    dataset_labels= 'labelsTr'
-    BASE_IMG_PATH=os.path.join(data_dir, dataset_train)
-    BASE_IMG_PATH_LABELS=os.path.join(data_dir, dataset_labels)
-
-    # getting list of paths for files
-    trainList = glob(BASE_IMG_PATH + '/**/*.nii', recursive=True)
-    labelList = glob(BASE_IMG_PATH_LABELS + '/**/*.nii', recursive=True)
-
-    print ( BASE_IMG_PATH + '\n' + "Number of images: {}". format (len (trainList)))
-    print ( BASE_IMG_PATH_LABELS + '\n' + "Number of images: {}". format (len (labelList)))
-    return trainList, labelList
-
-def metadata(arrays):
-    for arr in arrays:
-        print('shape: {}, min: {}, max: {}'.format(arr.shape, np.amin(arr), np.amax(arr)))
-
-def get_list_IDs(data_dir, val_split = 0.8):
+def get_list_IDs(data_dir, splits = [0.6, 0.2, 0.2]):
     """
-    param data_dir: data directory to the training or label folder
-    * assumes labels and training images have same names
+    Divides filenames into train/val/test sets
+    Args:
+        data_dir: file path to the directory of all the files; assumes labels and training images have same names
+        splits: a list with 3 elements corresponding to the decimal train/val/test splits; [train, val, test]
+    Returns:
+        a dictionary of file ids for each set
     """
     id_list = os.listdir(data_dir)
     total = len(id_list)
-    train = round(total * val_split)
-    return {'train': id_list[:train], 'val': id_list[train:]
+    train = round(total * splits[0])
+    val_split = round(total * splits[1]) + train
+    return {"train": id_list[:train], "val": id_list[train:val_split], "test": id_list[val_split:]
            }
 
 def sanity_checks(patch_x, patch_y):
-    '''
-    Checks for NaNs, and makes sure that the labels are one-hot encoded
-    '''
+    """
+    Checks for NaNs, and makes sure that the labels are one-hot encoded.
+    Args:
+        patch_x: a numpy array
+        patch_y: a numpy array (label)
+    Returns:
+        True (boolean) if all the asserts run.
+    """
     # sanity checks
-    assert not np.any(np.isnan(patch_x)) and not np.any(np.isnan(patch_y))
+    checks_nan_x, checks_nan_y = np.any(np.isnan(patch_x)), np.any(np.isnan(patch_y))
+    assert not checks_nan_x and not checks_nan_y # NaN checks
     assert np.array_equal(np.unique(patch_y), np.array([0,1])) or np.array_equal(np.unique(patch_y), np.array([0]))
     return True
 
 def add_channel(image):
-    '''
+    """
     Adds a single channel dimension to a 3D or 2D monomodal SimpleITK image or numpy array.
-    * channels_last
-    '''
+    Args:
+        image: a monomodal SimpleITK image or numpy array without a channel dimension
+    Returns:
+        A single channel numpy array (channels_last)
+    """
     if isinstance(image, sitk.Image):
         image = GetArrayFromImage(image)
     return np.expand_dims(image.squeeze(), -1).astype(np.float32)
-
-############## NOT IN USE
-def convert_4D_to_3D(arr_4D):
-    '''
-    arr_4D: sitk image (x,y,z, n_channels)
-    returns: list of 3D np arrays (x,y,z)
-    '''
-    arrays = []
-    np_img = sitk.GetArrayFromImage(arr_4D)
-    ### OPPORTUNITY TO GET 4D PATCHES HERE
-
-    # getting list of 3D SimpleITK Images for each modality/channel
-    for arr_3D in np.split(np_img, arr_4D.GetDimension(), axis= 0):
-        arrays.append(arr_3D.squeeze())
-    return arrays
-
-def to_channels_first(arr):
-    # converts 4D nd.array to channels first; (x,y,z, n_channels) to (x,n_channels,y,z)
-    if len(arr.shape) == 4:
-        return np.transpose(arr, [3, 0, 1,2])
-    elif len(arr.shape) == 3:
-        return np.expand_dims(arr, axis = 0)
