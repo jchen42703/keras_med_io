dd8bdcdf42780a51a649e66d71235d7c1843e354
Joseph <jchen42703@gmail.com>
2019-01-19 09:53:56 -0500

    Added documentation and random patch data augmentation tests for 2D and 3D

--- tests/generator_tests.py
+++ tests/generator_tests.py
@@ -77,6 +77,10 @@ class GeneratorTest(unittest.TestCase):
         self.assertEqual(x_3.shape, self.output_shape_3D)
 
     def test_bal_patch_2D(self):
+        """
+        Tests shapes for 2D balanced patch generation from the PosRandomPatchGenerator.
+        * Does not test the distribution, which is done through an eye test
+        """
         # instantiating generators
         mode = "bal"
         bal_gen_2D = PosRandomPatchGenerator(self.list_IDs, self.data_dirs, self.batch_size, self.patch_shape_2D,
@@ -87,11 +91,44 @@ class GeneratorTest(unittest.TestCase):
         self.assertEqual(x_2.shape, self.output_shape_2D)
 
     def test_bal_patch_3D(self):
+        """
+        Tests shapes for 3D balanced patch generation from the PosRandomPatchGenerator.
+        * Does not test the distribution, which is done through an eye test
+        """
         mode = "bal"
         bal_gen_3D = PosRandomPatchGenerator(self.list_IDs, self.data_dirs, self.batch_size, self.patch_shape_3D,
                                                self.n_channels, self.n_classes, mode = mode,)
         x_3, y_3 = bal_gen_3D.__getitem__(1)
         assert x_3.shape == y_3.shape
         self.assertEqual(x_3.shape, self.output_shape_3D)
+
+    def test_data_aug_rand_2D(self):
+        """
+        Tests shape for data augmentation for randomly generated 2D patches.
+        * Actual data aug tests are done by Isensee in the batchgenerators library.
+        """
+        # instantiating generators
+        mode = "rand"
+        rand_gen_2D = PosRandomPatchGenerator(self.list_IDs, self.data_dirs, self.batch_size, self.patch_shape_2D,
+                                               self.n_channels, self.n_classes, mode = mode, data_aug = True)
+        # testing their output shapes
+        x_2, y_2 = rand_gen_2D.__getitem__(1)
+        assert x_2.shape == y_2.shape
+        self.assertEqual(x_2.shape, self.output_shape_2D)
+
+    def test_data_aug_rand_3D(self):
+        """
+        Tests shape for data augmentation for randomly generated 3D patches.
+        * Actual data aug tests are done by Isensee in the batchgenerators library.
+        """
+        # instantiating generators
+        mode = "rand"
+        rand_gen_3D = PosRandomPatchGenerator(self.list_IDs, self.data_dirs, self.batch_size, self.patch_shape_3D,
+                                               self.n_channels, self.n_classes, mode = mode, data_aug = True)
+        # testing their output shapes
+        x_3, y_3 = rand_gen_3D.__getitem__(1)
+        assert x_3.shape == y_3.shape
+        self.assertEqual(x_3.shape, self.output_shape_3D)
+
 
 unittest.main(argv=[''], verbosity=2, exit=False)
