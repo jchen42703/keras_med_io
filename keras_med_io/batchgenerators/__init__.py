30a689a6b7c433f488b3d88befc167cf34a86c79
Joseph <jchen42703@gmail.com>
2019-02-12 18:53:19 -0500

    Getting rid of the local `batchgenerators` module.
    Since batchgenerators is now on PyPi, it makes more sense to actually use it as a dependency. Therefore, as a the first step to this transition, we need to get rid of the copied code.

--- keras_med_io/batchgenerators/__init__.py
+++
@@ -1,8 +0,0 @@
-# from __future__ import absolute_import
-#
-# import keras_med_io.batchgenerators.augmentations
-# import keras_med_io.batchgenerators.generators
-# from keras_med_io.batchgenerators.augmentations import utils
-# from keras_med_io.keras_med_io.batchgenerators.generators import noise_generators, resampling_augmentation_generators, \
-#     sample_normalization_generators, utility_generators
-# import keras_med_io.batchgenerators.transforms
