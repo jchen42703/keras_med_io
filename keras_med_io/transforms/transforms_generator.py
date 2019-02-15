8253f3f7548900ecfa71602fe4021be18a068016
Joseph <jchen42703@gmail.com>
2019-02-14 20:51:32 -0500

    Added the reshape function to pad inputs to the max patient shape for stacking
    This was because the regular numpy resize rearranged pixels instead of padding them. Additionally, some variable names were changed and added a new parameter `max_patient_shape` so that padding is reliable (the filler value was x_train.min() because the input data was already locally preprocessed and the filler value was no longer `0`).

--- keras_med_io/transforms/transforms_generator.py
+++ keras_med_io/transforms/transforms_generator.py
@@ -2,7 +2,7 @@ import numpy as np
 from keras_med_io.utils.gen_utils import BaseGenerator
 from keras_med_io.utils.patch_utils import PosRandomPatchExtractor
 from keras_med_io.utils.io_func import sanity_checks, add_channel, \
-                                       get_multi_class_labels, load_data
+                                       get_multi_class_labels, load_data, reshape
 import nibabel as nib
 from random import randint
 import os
@@ -19,18 +19,20 @@ class BaseTransformGenerator(BaseGenerator):
         data_dirs: list of paths to both the input dir and labels dir
         batch_size: The number of images you want in a single batch
         n_channels: number of channels
-        n_classes: number of classes for one hot encoding (>=2)
+        n_classes: number of unique labels excluding the background class (i.e. binary; n_classes = 1)
         ndim: number of dimensions of the input (excluding the batch_size and n_channels axes)
         transform (Transform instance): If you want to use multiple Transforms, use the Compose Transform.
+        max_patient_shape: a tuple representing the maximum patient shape in a dataset; i.e. (x,y, (z,))
         shuffle: boolean
     """
     def __init__(self, list_IDs, data_dirs, batch_size, n_channels, n_classes, ndim,
-                transform, shuffle = True):
+                transform = None, max_patient_shape = (144, 255, 319), shuffle = True):
 
         BaseGenerator.__init__(self, list_IDs = list_IDs, data_dirs = data_dirs, batch_size = batch_size,
                                n_channels = n_channels, n_classes = n_classes, shuffle = shuffle)
-        self.transform = transform
         self.ndim = ndim
+        self.transform = transform
+        self.max_patient_shape = max_patient_shape
         self.indexes = np.arange(len(self.list_IDs))
 
     def __getitem__(self, idx):
@@ -43,7 +45,9 @@ class BaseTransformGenerator(BaseGenerator):
         # Fetches batched IDs for a thread
         list_IDs_temp = [self.list_IDs[k] for k in indexes]
         X, Y = self.data_gen(list_IDs_temp)
-        return self._apply_transform(X, Y)
+        if self.transform is not None:
+            X, Y = self._apply_transform(X, Y)
+        return (X, Y)
 
     def _apply_transform(self, X, Y):
         data_dict = {}
@@ -64,23 +68,33 @@ class BaseTransformGenerator(BaseGenerator):
         Returns:
             tuple of two numpy arrays: x, y
         """
-        patches_x = []
-        patches_y = []
+        images_x = []
+        images_y = []
         for id in list_IDs_temp:
             # loads data as a numpy arr and then changes the type to float32
             x_train = load_data(os.path.join(self.data_dirs[0], id))
             y_train = load_data(os.path.join(self.data_dirs[1], id))
             if not x_train.shape[-1] == self.n_channels:
                 # Adds channel in case there is no channel dimension
-                x_train, y_train = add_channel(x_train), add_channel(y_train)
+                x_train = add_channel(x_train)
                 assert len(x_train.shape) == self.ndim + 1, "Input shape must be the \
                                                             shape (x,y, n_channels) or (x, y, z, n_channels)"
-            if self.n_classes > 2: # no point to run this when binary (foreground/background)
-                patch_y = get_multi_class_labels(patch_y, n_labels = self.n_classes, remove_background = True)
-            assert sanity_checks(patch_x, patch_y)
-            patches_x.append(patch_x), patches_y.append(patch_y)
+            if not y_train.shape[-1] == self.n_channels:
+                # Adds channel in case there is no channel dimension
+                y_train = add_channel(y_train)
+                assert len(y_train.shape) == self.ndim + 1, "Input labels must be the \
+                                                            shape (x,y, n_channels) or (x, y, z, n_channels)"
+            if self.n_classes > 1: # no point to run this when binary (foreground/background)
+                y_train = get_multi_class_labels(y_train, n_labels = self.n_classes, remove_background = True)
+            # Padding to the max patient shape (so the arrays can be stacked)
+            x_train = reshape(x_train, x_train.min(), self.max_patient_shape + (self.n_channels, ))
+            y_train = reshape(y_train, 0, self.max_patient_shape + (self.n_classes, ))
 
-        input_data, seg_masks = np.stack(patches_x), np.stack(patches_y)
+                # x_train.resize(max_patient_shape + (self.n_channels, )), y_train.resize(max_patient_shape + (self.n_classes, ))
+            assert sanity_checks(x_train, y_train)
+            images_x.append(x_train), images_y.append(y_train)
+
+        input_data, seg_masks = np.stack(images_x), np.stack(images_y)
         return (input_data, seg_masks)
 
     def _convert_dformat(self, arr, convert_to = "channels_last"):
