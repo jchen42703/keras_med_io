cd649f9a25894178745ebf3e9a46a4c56f9475c4
Joseph <jchen42703@gmail.com>
2019-03-28 11:07:21 -0400

    Made `undo_reshape_padding` compatible with orig_shape and ensured that pad_nonint_extraction requires data with channels included
    Small change is that now you can specify the pad_value in `pad_nonint_extraction` depending on the image (will most likely add edge support in the future with **kwargs). Also, I updated the documentation a bit.

--- keras_med_io/inference/infer_utils.py
+++ keras_med_io/inference/infer_utils.py
@@ -1,31 +1,31 @@
 import numpy as np
 
-def pad_nonint_extraction(image, orig_shape, coords):
+def pad_nonint_extraction(image, orig_shape, coords, pad_value = 0):
     """
-    Pads the cropped output from `io.io_utils`'s extract_nonint_region function
+    Pads the cropped output from `utils.shape_io`'s extract_nonint_region function
     Args:
-        image: either the mask or the thresholded (= 0.5) segmentation prediction (x, y, z)
-        orig_shape: Original shape of the 3D volume no channels
+        image: either the mask or the thresholded (= 0.5) segmentation prediction (x, y, z, n_channels)
+        orig_shape: Original shape of the 3D volume including the channels
         coords: outputted coordinates from `extract_nonint_region`
     Returns:
         padded: numpy array of shape `orig_shape`
     """
     # trying to reverse the cropping with padding
-    assert len(coords) == len(orig_shape), "Please make sure that the length of coords matches orig_shape."
-    padding = [[coords[i][0], orig_shape[i] - coords[i][1]] for i in range(len(orig_shape))]
-    padded = np.pad(image, padding, mode = 'constant', constant_values = 0)
+    assert (len(coords) + 1) == len(orig_shape), "Please make sure that orig_shape includes the channels dimension"
+    padding = [[coords[i][0], orig_shape[i]-coords[i][1]] for i in range(len(orig_shape[:-1]))] + [[0,0]]
+    padded = np.pad(image, padding, mode = 'constant', constant_values = pad_value)
     return padded
 
 def undo_reshape_padding(image, orig_shape):
     """
-    Undoes the padding done by the `reshape` function in `utils.io_func`
+    Undoes the padding done by the `reshape` function in `utils.shape_io`
     Args:
         image: numpy array that was reshaped to a larger size using reshape
         orig_shape: original shape before padding (do not include the channels)
     Returns:
         the reshaped image
     """
-    return image[:orig_img.shape[0], :orig_img.shape[1], :orig_img.shape[2]]
+    return image[:orig_shape[0], :orig_shape[1], :orig_shape[2]]
 
 # def load_data_predict():
 #     """
